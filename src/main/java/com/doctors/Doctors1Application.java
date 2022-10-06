2e226923a92ad12c6bbcee5c9a8263630cd2afdc
Camilo Ceballos <yatsude0@gmail.com>
2022-10-06 07:12:48 -0500

    first commit

---
+++ src/main/java/com/doctors/Doctors1Application.java
@@ -0,0 +1,13 @@
+package com.doctors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doctors1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Doctors1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/doctors/Doctors1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doctors1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
